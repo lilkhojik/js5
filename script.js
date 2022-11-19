6dcaf35b6c84d51d3cbf96345833891fbed747cd
test <test>
2022-11-20 01:24:52 +0500

    js5

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-var name = prompt("Ismingizni kiriting")
-console.log(" SIzning ismingiz " + name);
-var age = +prompt(" Yoshingiz nechida? ")
-console.log(' Siz ' + age + " yoshdasiz ");
-var misol1 = +prompt(' Misol 1: 6 + 3=? ')
-if (misol1 == 9) {
-    console.log(" Misol 1: sizning javobingiz to'g'ri " + misol1)
-} else {
-    console.log(" Misol 1: sizning javobingiz noto'g'ri " + misol1 + " To'g'ri javob:" + (6 + 3))
+let box = 5
+for (let a = 1;  a< 5; a++) {
+    box = box*a
+    
 }
-var misol2 = +prompt('Misol 2: 20 - 5 =? ')
-// console.log(' Misol 2: 20 - 5 = ' + (20 - 5) + " Sizning javob: " + misol2);
-if (misol2 == 20) {
-    console.log(" Misol 2: sizning javobingiz to'g'ri " + misol2);
-} else {
-    console.log(" Misol 2: sizning javobingiz noto'g'ri " + misol2 + " To'g'ri javob " + (20 - 5));
-}
-var misol3 = +prompt('Misol 3: 15 * 2 =? ')
-// console.log(' Misol 3: 15 * 2 = ' + 15 * 2 + " Sizning javob: " + misol3);
-if (misol3 == 30) {
-    console.log("Misol 3: sizning javobingiz to'g'ri" + misol3);
-} else {
-    console.log(" Misol 3: sizning javobingiz noto'g'ri " + misol3 + " To'g'ri javob " + (15 * 2));
-}
-var misol4 = +prompt('Misol 1: 8 / 4 =? ')
-// console.log(' Misol 4: 8 / 4 = ' + 8 / 4 + " Sizning javob: " + misol4);
-if (misol4 == 2) {
-    console.log(" Misol 4: sizning javobingiz to'g'ri " + misol4);
-} else {
-    console.log(" Misol 4: sizning javobingiz noto'g'ri " + misol4 + " To'g'ri javob " + (8 / 4));
-}
-var misol5 = +prompt('Misol 5: 10 % 3 =? ')
-// console.log(' Misol 5: 10 % 3 = ' + 10 % 3 + " Sizning javob: " + misol5);
-if (misol5 == 1) {
-    console.log(" Misol 5: sizning javobingiz to'g'ri " + misol5);
-}else{
-    console.log(" Misol 5: sizning javobingiz noto'g'ri " + misol5 + " To'g'ri javob " + (10%3));
-}
-var end = alert('Kodlarga kirib javoblarni taqqoslang')
-console.log('end');
+console.log(box);
